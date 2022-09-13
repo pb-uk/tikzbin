d0b6140107308329bf19fd911e8cac82523a7de4
pb-uk <23248021+pb-uk@users.noreply.github.com>
2022-09-13 12:42:19 +0100

    chore: minify with terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,28 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+import pkg from './package.json';
+
+// Human timestamp for banner.
+const datetime = new Date().toISOString().substring(0, 19).replace('T', ' ');
+
+const pkgName = pkg.name.replace(/@.*\//, '');
+
+// Main banner.
+const banner = `/*! ${pkgName} v${pkg.version} ${datetime}
+ *! ${pkg.homepage}
+ *! Copyright ${pkg.author} ${pkg.license} license.
+ */
+`;
 
 export default {
   input: './src/index.ts',
   output: {
     file: './docs/index.min.js',
     format: 'iife',
+    banner,
+    sourcemap: true,
   },
-  plugins: [nodeResolve(), typescript()],
+  plugins: [nodeResolve(), typescript(), terser()],
 };
