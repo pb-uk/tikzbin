776d65fcff594a1cc9f5dc2c36f22a975ec8bed3
pb-uk <23248021+pb-uk@users.noreply.github.com>
2022-09-13 12:11:56 +0100

    feat: png download

---
+++ src/image-conversion.ts
@@ -0,0 +1,1 @@
+// https://spin.atomicobject.com/2014/01/21/convert-svg-to-png/
--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,42 @@ import { stex } from '@codemirror/legacy-modes/mode/stex';
 
 let lastSource: string;
 
+const getDownloadFileName = (ext: string): string => {
+  return (
+    (<HTMLInputElement>document.getElementById('input-filename')).value +
+    `.${ext}`
+  );
+};
+
+const enableDownloadButtons = (enable = true) => {
+  if (enable) {
+    document.getElementById('btn-png')?.removeAttribute('disabled');
+    document.getElementById('btn-svg')?.removeAttribute('disabled');
+    // document.getElementById('btn-jpg')?.removeAttribute('disabled');
+    return;
+  }
+  document.getElementById('btn-png')?.setAttribute('disabled', 'disabled');
+  document.getElementById('btn-svg')?.setAttribute('disabled', 'disabled');
+  // document.getElementById('btn-jpg')?.setAttribute('disabled', 'disabled');
+};
+
+// https://spin.atomicobject.com/2014/01/21/convert-svg-to-png/
+
 // const sourceChanged = window.sourceChanged = async () => {
 // eslint-disable-next-line
 // @ts-ignore
 window.sourceChanged = () => {
   // Check the source has actually changed.
   const newSource = editor.state.doc.toString();
-  console.log('woo', newSource, lastSource);
   if (lastSource === newSource) return;
+  enableDownloadButtons(false);
   lastSource = newSource;
   const scriptEl = document.createElement('script');
   scriptEl.innerHTML = newSource;
   scriptEl.setAttribute('type', 'text/tikz');
   document.getElementById('rendered')?.replaceChildren(scriptEl);
   window.onload && window.onload(new Event('app'));
+  enableDownloadButtons();
 };
 
 const editor = new EditorView({
@@ -46,22 +68,72 @@ function debounce(func, timeout = 300) {
 }
 */
 
+const captureSvgAsDataUri = (): string => {
+  const renderedEl = document.getElementById('rendered');
+  if (renderedEl == null || renderedEl.firstChild == null) return '';
+  const svg = (<HTMLElement>renderedEl.firstChild).innerHTML;
+  return (
+    'data:image/svg+xml;base64,' +
+    window.btoa(
+      svg.replace(
+        '<svg',
+        '<?xml version="1.0"?>\n<svg xmlns="http://www.w3.org/2000/svg"'
+      )
+    )
+  );
+};
+
 // eslint-disable-next-line
 // @ts-ignore
 window.captureSvg = () => {
-  const renderedEl = document.getElementById('rendered');
-  if (renderedEl == null || renderedEl.firstChild == null) return;
-  const svg = (<HTMLElement>renderedEl.firstChild).innerHTML;
-  const base64 =
-    'data:image/svg+xml;base64,' +
-    btoa(
-      svg.replace(
-        '<svg',
-        '<?xml version="1.0" encoding="UTF-8" ?>\n<svg xmlns="http://www.w3.org/2000/svg"'
-      )
-    );
+  const dataUri = captureSvgAsDataUri();
   const downloadLink = document.createElement('a');
-  downloadLink.href = base64;
-  downloadLink.download = 'tikz.svg';
+  downloadLink.href = dataUri;
+  downloadLink.download = getDownloadFileName('svg');
   downloadLink.click();
 };
+
+// eslint-disable-next-line
+// @ts-ignore
+window.capturePng = async () => {
+  const image = document.createElement('img');
+  image.src = captureSvgAsDataUri();
+  image.onload = () => {
+    const canvas = document.createElement('canvas');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    const context = canvas.getContext('2d');
+    if (context === null) return;
+    context.drawImage(image, 0, 0);
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = canvas.toDataURL('image/png');
+    downloadLink.download = getDownloadFileName('png');
+    downloadLink.click();
+  };
+};
+
+/* Jpeg suffers from a black background.
+// eslint-disable-next-line
+// @ts-ignore
+window.captureJpeg = async () => {
+  const image = document.createElement('img');
+  image.src = captureSvgAsDataUri();
+  image.style.backgroundColor = "#ffffff";
+  image.onload = () => {
+    const canvas = document.createElement('canvas');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    const context = canvas.getContext('2d');
+    if (context === null) return;
+    context.drawImage(image, 0, 0);
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = canvas.toDataURL('image/jpeg');
+    downloadLink.download = getDownloadFileName('jpg');
+    downloadLink.click();
+  };
+};
+*/
+
+enableDownloadButtons(false);
